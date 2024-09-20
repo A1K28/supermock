5917a22971e8a5fb0fd6785a7869c8152de63b02
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-09-20 13:51:16 +0400

    update assertEquals

--- src/main/java/com/github/a1k28/supermock/Assertions.java
+++ src/main/java/com/github/a1k28/supermock/Assertions.java
@@ -1,11 +1,12 @@
 package com.github.a1k28.supermock;
 
-import com.google.gson.Gson;
-
 public class Assertions {
     public static void assertEquals(Object o1, Object o2) {
-        if (o1 == o2) return;
         String errorMessage = "Objects are not equal.\nExpected: " + o1 + "\n" + "Got: " + o2;
+        if (o1 == null && o2 == null) return;
+        if (o1 == null ^ o2 == null)
+            throw new AssertionError(errorMessage);
+        if (o1 == o2) return;
         if (o1.getClass() != o2.getClass())
             throw new AssertionError(errorMessage);
         if (!Parser.serialize(o1).equals(Parser.serialize(o2)))
